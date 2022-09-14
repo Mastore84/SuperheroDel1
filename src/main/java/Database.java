feea23c655bcb1a7fd338da5fb5e964745dc30f9
Mastore84 <mpb.petersen@gmail.com>
2022-09-14 15:59:13 +0200

    Fixed isHuman variable comparison

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -2,11 +2,11 @@ public class Database {
 
     Superhero[] database = new Superhero[4];
 
-    int database_Length = 0;
+    int databaseCount = 0;
 
     public void addSuperhero(String superheroName, String realName, int yearCreated, String superPowers, boolean isHuman, double strength){
-        database[database_Length] = new Superhero(superheroName, realName, yearCreated, superPowers, isHuman, strength);
-        System.out.println(database[database_Length]);
-        database_Length++;
+        database[databaseCount] = new Superhero(superheroName, realName, yearCreated, superPowers, isHuman, strength);
+        System.out.println(database[databaseCount]);
+        databaseCount++;
     }
 }
