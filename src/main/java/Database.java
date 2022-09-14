faa5fd459ab85dcc6e7c651e3c0da854d34ede15
Mastore84 <mpb.petersen@gmail.com>
2022-09-14 15:30:54 +0200

    (Kind of) working Database class

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,9 +1,12 @@
 public class Database {
 
-    private Object[] database;
+    Superhero[] superhero = new Superhero[4];
+
+    int database_Length = 0;
 
     public void addSuperhero(String superheroName, String realName, int yearCreated, String superPowers, boolean isHuman, double strength){
-        Superhero superhero = new Superhero(superheroName, realName, yearCreated, superPowers, isHuman, strength);
-        //database[database.length].(superhero);
+        superhero[database_Length] = new Superhero(superheroName, realName, yearCreated, superPowers, isHuman, strength);
+        System.out.println(superhero[database_Length]);
+        database_Length++;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Database[] database = new Database[4];
-
+        //Superhero[] database = new Superhero[4];
+        Database database = new Database();
         boolean isHuman = true;
         Scanner sc = new Scanner(System.in);
         System.out.println("Indtast superheltenavn: ");
@@ -25,11 +25,7 @@ public class Main {
         }
         System.out.println("Indtast styrke (tal med to decimaler): ");
         double strength = sc.nextDouble();
-        Superhero superhero = new Superhero(superheroName, realName, yearCreated, superPowers, isHuman, strength);
-        //database.addSuperhero(superheroName, realName, yearCreated, superPowers, isHuman, strength);
+        database.addSuperhero(superheroName, realName, yearCreated, superPowers, isHuman, strength);
 
-
-
-        System.out.println(superhero.toString());
     }
 }
