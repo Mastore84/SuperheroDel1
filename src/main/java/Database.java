753e7ceb995ad2fa2e670cfc534f9596b04e5c6e
Mastore84 <mpb.petersen@gmail.com>
2022-09-14 15:49:21 +0200

    Fixed isHuman variable comparison

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,12 +1,12 @@
 public class Database {
 
-    Superhero[] superhero = new Superhero[4];
+    Superhero[] database = new Superhero[4];
 
     int database_Length = 0;
 
     public void addSuperhero(String superheroName, String realName, int yearCreated, String superPowers, boolean isHuman, double strength){
-        superhero[database_Length] = new Superhero(superheroName, realName, yearCreated, superPowers, isHuman, strength);
-        System.out.println(superhero[database_Length]);
+        database[database_Length] = new Superhero(superheroName, realName, yearCreated, superPowers, isHuman, strength);
+        System.out.println(database[database_Length]);
         database_Length++;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Superhero[] database = new Superhero[4];
+        boolean isHuman;
         Database database = new Database();
-        boolean isHuman = true;
         Scanner sc = new Scanner(System.in);
         System.out.println("Indtast superheltenavn: ");
         String superheroName = sc.nextLine();
@@ -16,11 +15,11 @@ public class Main {
         System.out.println("Indtast superkrafter: ");
         String superPowers = sc.nextLine();
         System.out.println("Er et menneske? j/n: ");
-        String isHumanQ = sc.next();
-        if (isHumanQ == "j"){
+        String isHumanQ = sc.nextLine();
+        if (isHumanQ.equalsIgnoreCase( "j")){
             isHuman = true;
         }
-        else if (isHumanQ == "n"){
+        else {
             isHuman = false;
         }
         System.out.println("Indtast styrke (tal med to decimaler): ");
